1d99d33d66c5c6cc77086ad1d5d19159904490c2
Luqsonic <62737177+Luqsonic@users.noreply.github.com>
2021-11-09 21:13:29 +0300

    Update app.py

--- App/server/app.py
+++ App/server/app.py
@@ -16,8 +16,8 @@ async def root_route_handler(request):
                               "version": 'bdnddn'})
 
 
-@routes.get(r"/{message_id:\S+}")
+@routes.get(r"/{message_id}")
 async def stream_handler(request):
     message_id = request.match_info['message_id']
-    message_id = int(re.search(r'(\d+)(?:\/\S+)?', message_id).group(1))
+    web.Response(text="Fuck youyy")
      
